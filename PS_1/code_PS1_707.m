a7b42942fdee2aff53e5acc807543436820b5037
Verónica Pérez <71244136+VeronicaCPerez@users.noreply.github.com>
2023-09-20 14:31:53 -0400

    Update code_PS1_707.m

--- PS_1/code_PS1_707.m
+++ PS_1/code_PS1_707.m
@@ -39,8 +39,7 @@ xlabel('X');
 ylabel('Y');
 zlabel('H');
 set(gcf, 'Color', 'w'); % Set the background color to white
-
-saveas()
+saveas(gcf, '/Users/veronica/Dropbox/PhD/2023_2/EC_707_Stats/problem_sets_707/PS_1/H_normal.png'); % Save as PNG
 
 % Create Figure 2: H_plus
 figure;
@@ -50,6 +49,7 @@ xlabel('X');
 ylabel('Y');
 zlabel('H Plus');
 set(gcf, 'Color', 'w'); % Set the background color to white
+saveas(gcf, '/Users/veronica/Dropbox/PhD/2023_2/EC_707_Stats/problem_sets_707/PS_1/H_plus_normal.png'); % Save as PNG
 
 % Create Figure 3: H_minus
 figure;
@@ -59,3 +59,66 @@ xlabel('X');
 ylabel('Y');
 zlabel('H Minus');
 set(gcf, 'Color', 'w'); % Set the background color to white
+saveas(gcf, '/Users/veronica/Dropbox/PhD/2023_2/EC_707_Stats/problem_sets_707/PS_1/H_minus_normal.png'); % Save as PNG
+
+
+%%% with uniform dist
+%%%%%%%%%%%%%%%%%%%%%%
+
+% Define the parameters for the uniform distribution
+a_x = -2;       % Lower bound of X
+b_x = 2;        % Upper bound of X
+
+a_y = -2;       % Lower bound of Y
+b_y = 2;        % Upper bound of Y
+
+% Create a range of values for x and y
+x = linspace(a_x, b_x, 100); % Adjust the range and number of points as needed
+y = linspace(a_y, b_y, 100);
+
+% Create a grid of (x, y) pairs for evaluation
+[X, Y] = meshgrid(x, y);
+
+% Compute the CDF values for X and Y (uniform distribution)
+F_x = (X - a_x) / (b_x - a_x);
+F_y = (Y - a_y) / (b_y - a_y);
+
+% Compute the joint CDF H(x, y)
+H = F_x .* F_y;
+
+% Compute H_plus
+H_plus = min(F_x, F_y);
+
+% Compute H_minus
+H_minus = max(F_x + F_y - 1, 0);
+
+% Create Figure 1: Joint CDF (H)
+figure;
+surf(X, Y, H);
+title('Joint CDF (H)');
+xlabel('X');
+ylabel('Y');
+zlabel('H');
+set(gcf, 'Color', 'w'); % Set the background color to white
+saveas(gcf, '/Users/veronica/Dropbox/PhD/2023_2/EC_707_Stats/problem_sets_707/PS_1/H_unf.png'); % Save as PNG
+
+% Create Figure 2: H_plus
+figure;
+surf(X, Y, H_plus);
+title('H Plus');
+xlabel('X');
+ylabel('Y');
+zlabel('H Plus');
+set(gcf, 'Color', 'w'); % Set the background color to white
+saveas(gcf, '/Users/veronica/Dropbox/PhD/2023_2/EC_707_Stats/problem_sets_707/PS_1/H_plus_unf.png'); % Save as PNG
+
+% Create Figure 3: H_minus
+figure;
+surf(X, Y, H_minus);
+title('H Minus');
+xlabel('X');
+ylabel('Y');
+zlabel('H Minus');
+set(gcf, 'Color', 'w'); % Set the background color to white
+saveas(gcf, '/Users/veronica/Dropbox/PhD/2023_2/EC_707_Stats/problem_sets_707/PS_1/H_minus_unf.png'); % Save as PNG
+
